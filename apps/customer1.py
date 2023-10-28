718213811ee19df616cbec95eb84b0e9212f5316
tapiwa chamboko <80702706+tapiwachamb@users.noreply.github.com>
2023-10-28 19:27:04 +0200

    Update customer1.py

--- apps/customer1.py
+++ apps/customer1.py
@@ -1,7 +1,6 @@
 from pycaret.classification import load_model, predict_model
 import streamlit as st
 import pandas as pd
-import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
 import plotly.express as px
